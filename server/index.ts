0603261818e7966c63f945c9d6f2fd449c0ca94c
jjnr <31824256+jrodri06@users.noreply.github.com>
2021-02-19 19:13:50 +0100

    tests to get db to deploy

--- server/index.ts
+++ server/index.ts
@@ -10,41 +10,39 @@ import routes from './routes';
 import ToDo from './models/ToDo';
 
 
-config();
-const app = express();
-
-app.use(cors({
-    "origin": "*",
-    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-    "preflightContinue": false,
-    "optionsSuccessStatus": 204
-}));
-app.use(bodyParser.json());
-app.use(methodOverride('_method'));
-
-mongoose.set('useFindAndModify', false);
-
-const connect = () => {
-    return mongoose.connect(process.env.DB_CONNECT!, { 
-        useUnifiedTopology: true,
-        useNewUrlParser: true 
-    }) 
-        .then(() => console.log('Connected to db!'))
-        .catch(err => console.log(`Error occurred! ${err.message}`));
-}
-
-app.use(routes);
-
-app.listen(4001, () => console.log(`Server is running on port 4001`));
-
-const handler = (event: any, context: any) => {
+module.exports.handler = (event: any, context: any) => {
     context.callbackWaitsForEmptyEventLoop = false;
+
+    config();
+    const app = express();
+
+    app.use(cors({
+        "origin": "*",
+        "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+        "preflightContinue": false,
+        "optionsSuccessStatus": 204
+    }));
+    app.use(bodyParser.json());
+    app.use(methodOverride('_method'));
+
+    mongoose.set('useFindAndModify', false);
+
+    const connect = () => {
+        return mongoose.connect(process.env.DB_CONNECT!, { 
+            useUnifiedTopology: true,
+            useNewUrlParser: true 
+        }) 
+            .then(() => console.log('Connected to db!'))
+            .catch(err => console.log(`Error occurred! ${err.message}`));
+    }
+
+    app.use(routes);
+
+    app.listen(4001, () => console.log(`Server is running on port 4001`));
 
     connect()
         .then(async () => await ToDo.find({}))
         .catch(err => {
             throw Error(`This is what you got ${err}`)
         });
-}
-
-module.exports.handler = handler;
+};
