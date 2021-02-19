cdc03a8b534b461463d8fc08c4eb2f2aa2219116
jjnr <31824256+jrodri06@users.noreply.github.com>
2021-02-19 18:58:59 +0100

    tests to get db to deploy

--- server/index.ts
+++ server/index.ts
@@ -4,9 +4,11 @@ import bodyParser from 'body-parser';
 import { config } from 'dotenv';
 import mongoose from 'mongoose';
 import methodOverride from 'method-override';
-// import serverless from 'serverless-http';
 
 import routes from './routes';
+
+import ToDo from './models/ToDo';
+
 
 config();
 const app = express();
@@ -21,15 +23,26 @@ app.use(bodyParser.json());
 app.use(methodOverride('_method'));
 
 mongoose.set('useFindAndModify', false);
-mongoose.connect(process.env.DB_CONNECT!, { 
-    useUnifiedTopology: true,
-    useNewUrlParser: true 
-}) 
-    .then(() => console.log('Connected to db!'))
-    .catch(err => console.log(`Error occurred! ${err.message}`));
+
+const connect = () => {
+    return mongoose.connect(process.env.DB_CONNECT!, { 
+        useUnifiedTopology: true,
+        useNewUrlParser: true 
+    }) 
+        .then(() => console.log('Connected to db!'))
+        .catch(err => console.log(`Error occurred! ${err.message}`));
+}
 
 app.use(routes);
-// app.use('/.netlify/functions/', routes);
-// const server = serverless(app);
 
 app.listen(4001, () => console.log(`Server is running on port 4001`));
+
+export default (event: any, context: any) => {
+    context.callbackWaitsForEmptyEventLoop = false;
+
+    connect()
+        .then(async () => await ToDo.find({}))
+        .catch(err => {
+            throw Error(`This is what you got ${err}`)
+        });
+}
