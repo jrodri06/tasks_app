4bdf43a03e73ecaa4d0f53c23e9774591ccc8821
jjnr <31824256+jrodri06@users.noreply.github.com>
2021-02-19 23:45:51 +0100

    changes in index file for server

--- server/ts/index.ts
+++ server/ts/index.ts
@@ -7,42 +7,22 @@ import methodOverride from 'method-override';
 
 import routes from './routes';
 
-import ToDo from './models/ToDo';
-
-
-module.exports.handler = (event: any, context: any) => {
-    context.callbackWaitsForEmptyEventLoop = false;
-
-    config();
-    const app = express();
-
-    app.use(cors({
-        "origin": "*",
-        "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-        "preflightContinue": false,
-        "optionsSuccessStatus": 204
-    }));
-    app.use(bodyParser.json());
-    app.use(methodOverride('_method'));
-
-    mongoose.set('useFindAndModify', false);
-
-    const connect = () => {
-        return mongoose.connect(process.env.DB_CONNECT!, { 
-            useUnifiedTopology: true,
-            useNewUrlParser: true 
-        }) 
-            .then(() => console.log('Connected to db!'))
-            .catch(err => console.log(`Error occurred! ${err.message}`));
-    }
-
-    app.use(routes);
-
-    app.listen(4001, () => console.log(`Server is running on port 4001`));
-
-    connect()
-        .then(async () => await ToDo.find({}))
-        .catch(err => {
-            throw Error(`This is what you got ${err}`)
-        });
-};
+config();
+const app = express();
+app.use(cors({
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
+}));
+app.use(bodyParser.json());
+app.use(methodOverride('_method'));
+mongoose.set('useFindAndModify', false);
+mongoose.connect(process.env.DB_CONNECT!, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+})
+    .then(() => console.log('Connected to db!'))
+    .catch(err => console.log(`Error occurred! ${err.message}`));
+app.use(routes);
+app.listen(4001, () => console.log(`Server is running on port 4001`));
