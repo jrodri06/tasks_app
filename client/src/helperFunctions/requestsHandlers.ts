8fcf6284b9057cc1ec8ee694da0da5bfd5656636
jjnr <31824256+jrodri06@users.noreply.github.com>
2021-02-20 14:23:01 +0100

    process environment in requestHandlers and fix getPricesTotal

--- client/src/helperFunctions/requestsHandlers.ts
+++ client/src/helperFunctions/requestsHandlers.ts
@@ -3,6 +3,14 @@ import swal from 'sweetalert';
 import { offlineService, allQueues } from './offlineHandlers';
 import { localTasks } from './localStorageHandlers';
 
+console.log(process.env.NODE_ENV);
+console.log(process.env.NODE_ENV === 'development');
+
+const localHost = process.env.NODE_ENV === 'development' ? 'http://localhost:4001' : '';
+
+console.log('Local Host var');
+console.log(localHost);
+
 // Post Requests for create, update and delete
 const handlePostWhenOffline = async (
     details: any, 
@@ -82,7 +90,7 @@ export const submitCUDInfo = async (
 };
 
 export const formPath = (userData: object, cb: Function) => {
-    submitCUDInfo(`/task/new-todo`, userData, 'createUpdate')
+    submitCUDInfo(`${localHost}/task/new-todo`, userData, 'createUpdate')
         .then(data => {
             if(Object.keys(data).length > 0) {
                 // Offline scenario
@@ -107,7 +115,7 @@ export const collectToDos = async (cb: Function) => {
     cb(localTasks.getTasks());
 
     try {
-        const response = await fetch('/task/all-tasks');
+        const response = await fetch(`${localHost}/task/all-tasks`);
         const data = await response.json();
 
         if(response.status === 500 || response.status === 400 || response.status === 404){
@@ -126,7 +134,7 @@ export const collectToDos = async (cb: Function) => {
 // To double check
 export const convertSubToMain = async (id: string) => {
     try {
-        const converted: any = submitCUDInfo('/task/remove-subtask', { id }, 'erasure')
+        const converted: any = submitCUDInfo(`${localHost}/task/remove-subtask`, { id }, 'erasure')
             .then(res => res.json())
             .then(data => {
                 const { price, name, done } = data;
@@ -141,7 +149,7 @@ export const convertSubToMain = async (id: string) => {
             })
             .catch(err => console.error(err))
     
-        return await submitCUDInfo(`/task/new-todo`, converted, 'createUpdate');
+        return await submitCUDInfo(`${localHost}/task/new-todo`, converted, 'createUpdate');
 
     } catch(err) {
         console.error(err);
@@ -153,7 +161,7 @@ export const eraseTask = async (id: string) => {
     localTasks.removeTaskFromList({ id });
 
     try {
-        const response = await submitCUDInfo('/task/erase-task', { id }, 'erasure');
+        const response = await submitCUDInfo(`${localHost}/task/erase-task`, { id }, 'erasure');
         return response;
     } catch(err) {
         console.error(err);
@@ -167,7 +175,7 @@ export const updateDoneStatus = async (dataUpdated: {
     localTasks.updateTaskFromList(dataUpdated);
 
     try {
-        const response = await submitCUDInfo('/task/update-task', dataUpdated, 'createUpdate');
+        const response = await submitCUDInfo(`${localHost}/task/update-task`, dataUpdated, 'createUpdate');
         return response.status;
     } catch(err) {
         console.error(err);
@@ -184,7 +192,7 @@ export const createSubTask = async (subtask: {
     localTasks.createSubtaskToList(subtask);
 
     try {
-        await submitCUDInfo('/task/new-subtask', subtask, 'createUpdate');
+        await submitCUDInfo(`${localHost}/task/new-subtask`, subtask, 'createUpdate');
 
         swal('Added!', `The subtask "${subtask.name}" has been added`, 'success');    
     }  catch(err) {
@@ -196,7 +204,7 @@ export const updateSubTaskDone = async (subtask: Object) => {
     localTasks.updateSubtaskFromList(subtask);
 
     try { 
-        await submitCUDInfo(`/task/update-subtask`, subtask, 'createUpdate');
+        await submitCUDInfo(`${localHost}/task/update-subtask`, subtask, 'createUpdate');
     }  catch(err) {
         console.error(err);
     }
@@ -204,17 +212,21 @@ export const updateSubTaskDone = async (subtask: Object) => {
 
 export const getPricesTotal = (taskId: String) => {
     const currentTasks = localTasks.getTasks();
-    const selectedTask = currentTasks.find((task: { _id: string }) => task._id === taskId);
 
+    const selectedTask = currentTasks.find((task: { _id: string }) => task._id === taskId);
     let totalPrice = 0;
 
-    selectedTask.subtask.forEach((subtask: { price: string }) => {
-        if(subtask.price !== null) {
-            totalPrice = totalPrice + +subtask.price;
-        }
-    })
-
-    return totalPrice;
+    if(selectedTask === undefined) {
+        return totalPrice;
+    } else {
+        selectedTask.subtask.forEach((subtask: { price: string }) => {
+            if(subtask.price !== null) {
+                totalPrice = totalPrice + +subtask.price;
+            }
+        })
+    
+        return totalPrice;
+    }
 }
 
 export const getTask = async (taskId: String) => {
@@ -222,7 +234,7 @@ export const getTask = async (taskId: String) => {
     console.log(taskId);
 
     try {
-        const response = await fetch(`/edit-task/${taskId}`);
+        const response = await fetch(`${localHost}/edit-task/${taskId}`);
         const data = await response.json();
 
         if(response.status === 500 || response.status === 404 || response.status === 400) {
@@ -240,7 +252,7 @@ export const getTask = async (taskId: String) => {
 
 export const editTask = async (task: Object,  cb: Function) => {
     try {
-        const response = await fetch('/edit-task/', {
+        const response = await fetch(`${localHost}/edit-task/`, {
             method: "POST",
             headers: {
                 'Content-Type': 'application/json'
